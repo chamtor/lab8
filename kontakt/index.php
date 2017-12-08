ef1bdc05210d92b148085c2180b7adbdd0858e1a
chamtor <34063794+chamtor@users.noreply.github.com>
2017-12-08 23:54:49 +0100

    v1
    v1

---
+++ kontakt/index.php
@@ -0,0 +1,50 @@
+<?php
+session_start();
+session_unset();
+?>
+<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\">
+<html xmlns=\"http://www.w3.org/1999/xhtml\" lang=\"pl-PL\">
+<head>
+    <meta http-equiv="refresh" content="1000" /> 
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <script type="text/javascript"></script>
+      
+    <title>Zdunowski</title>
+
+
+</head>
+
+<body>
+<div id="naglowek">
+	<img src="img.jpg">
+</div>
+
+<div id="mySidenav" class="sidenav">
+  <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+  <a href="#">Strona głowna</a>
+  <a href="/kontakt/">Kontakt</a>
+  <a href="./mapa/mapa.php">Jak do nas dotrzeć</a>
+</div>
+
+<!--<span onclick="openNav()">open</span>-->
+<span style="font-size:30px;cursor:pointer" onclick="openNav()">&#9776; Open Menu</span>
+
+
+
+<div id="srodek">
+
+
+</div>
+
+<script>
+function openNav() {
+    document.getElementById("mySidenav").style.width = "200px";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
+</script>
+</body>
+</html>
